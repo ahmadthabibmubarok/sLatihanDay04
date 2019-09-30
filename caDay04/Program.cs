c6efa2fac091740c400052965ca65162645cafd4
ahmadthabibmubarok <ahmadthabibmubarok@gmail.com>
2019-09-30 10:43:28 +0700

    first commit

---
+++ caDay04/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caDay04
+{
+    public class Program
+    {
+        public static void Main(string[] args) //public tidak ditulis,otomatis termasuk public
+        {//perintah untuk cetak hasil
+            Console.WriteLine("Selamat datang batch 215");
+            // trick tunggu input, agar tidak otomatis close
+            Console.ReadLine();
+        }
+    }
+}
